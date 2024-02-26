9412a5a71eb6aab511cd6af1f9dfac5212b51398
browaeysrobin <robin.browaeys@ugent.be>
2024-02-26 16:16:06 +0100

    Add omnipath information to the plots

--- R/plotting.R
+++ R/plotting.R
@@ -290,6 +290,220 @@ make_sample_lr_prod_activity_plots = function(prioritization_tables, prioritized
   return(p)
   
 }
+#' @title make_sample_lr_prod_activity_plots_Omnipath
+#'
+#' @description \code{make_sample_lr_prod_activity_plots_Omnipath}  Visualize the scaled product of Ligand-Receptor (pseudobulk) expression per sample, and compare the different groups. In addition, show the NicheNet ligand activities in each receiver-celltype combination, AND the Omnipath curation scores of the LR pairs.
+#' @usage make_sample_lr_prod_activity_plots_Omnipath(prioritization_tables, prioritized_tbl_oi, widths = NULL)
+#'
+#' @inheritParams make_sample_lr_prod_activity_plots
+#' @param widths Vector of 4 elements: Width of the LR exprs product panel,  width of the scaled ligand activity panel, width of the cell-type specificity panel & width of the Omnipath panel. Default NULL: automatically defined based on nr of samples vs nr of group-receiver combinations. If manual change: example format: c(6,2,2,1) 
+#' @param prioritized_tbl_oi Should be the same as in `make_sample_lr_prod_activity_plots`, except, there should be Omnipath LR quality metrics included: curation_effort, n_resources, n_references. See example here, and vignettes, on how to do this.
+#'
+#' @return Ligand-Receptor Expression Product & Ligand Activities Dotplot/Heatmap 
+#'
+#' @import ggplot2
+#' @import dplyr
+#' @importFrom RColorBrewer brewer.pal
+#' @importFrom patchwork wrap_plots
+#' @importFrom viridis scale_color_viridis
+#'
+#' @examples
+#' \dontrun{
+#' library(dplyr)
+#' lr_network = readRDS(url("https://zenodo.org/record/3260758/files/lr_network.rds"))
+#' lr_network = lr_network %>% dplyr::rename(ligand = from, receptor = to) %>% dplyr::distinct(ligand, receptor)
+#' lr_network_all = readRDS("../../../NicheNet_V2/networks/data/ligand_receptor/lr_network_human_allInfo_30112033.rds") %>% mutate(ligand = convert_alias_to_symbols(ligand, organism = "human"), receptor = convert_alias_to_symbols(receptor, organism = "human"))
+#' ligand_target_matrix = readRDS(url("https://zenodo.org/record/3260758/files/ligand_target_matrix.rds"))
+#' sample_id = "tumor"
+#' group_id = "pEMT"
+#' celltype_id = "celltype"
+#' batches = NA
+#' contrasts_oi = c("'High-Low','Low-High'")
+#' contrast_tbl = tibble(contrast = c("High-Low","Low-High"), group = c("High","Low"))
+#' output = multi_nichenet_analysis(
+#'      sce = sce, 
+#'      celltype_id = celltype_id, 
+#'      sample_id = sample_id, 
+#'      group_id = group_id,
+#'      batches = batches,
+#'      lr_network = lr_network, 
+#'      ligand_target_matrix = ligand_target_matrix, 
+#'      contrasts_oi = contrasts_oi, 
+#'      contrast_tbl = contrast_tbl
+#'      
+#'      )
+#' group_oi = "High"
+#' prioritized_tbl_oi = output$prioritization_tables$group_prioritization_tbl %>% filter(fraction_expressing_ligand_receptor > 0) %>% filter(group == group_oi) %>% top_n(50, prioritization_score) 
+#' plot_oi = make_sample_lr_prod_activity_plots_Omnipath(output$prioritization_tables, prioritized_tbl_oi %>% inner_join(lr_network_all))
+#' plot_oi
+#' }
+#'
+#' @export
+#'
+make_sample_lr_prod_activity_plots_Omnipath = function(prioritization_tables, prioritized_tbl_oi, widths = NULL){
+  requireNamespace("dplyr")
+  requireNamespace("ggplot2")
+  
+  sample_data = prioritization_tables$sample_prioritization_tbl %>% dplyr::filter(id %in% prioritized_tbl_oi$id) %>% dplyr::mutate(sender_receiver = paste(sender, receiver, sep = " --> "), lr_interaction = paste(ligand, receptor, sep = " - "))   %>%  dplyr::arrange(receiver) %>% dplyr::group_by(receiver) %>%  dplyr::arrange(sender, .by_group = TRUE)
+  sample_data = sample_data %>% dplyr::mutate(sender_receiver = factor(sender_receiver, levels = sample_data$sender_receiver %>% unique()))
+  
+  group_data = prioritization_tables$group_prioritization_table_source  %>% dplyr::mutate(sender_receiver = paste(sender, receiver, sep = " --> "), lr_interaction = paste(ligand, receptor, sep = " - "))  %>% dplyr::distinct(id, sender, receiver, sender_receiver, lr_interaction, group, activity, activity_scaled, direction_regulation, prioritization_score) %>% dplyr::filter(id %in% sample_data$id) %>%  dplyr::arrange(receiver) %>% dplyr::group_by(receiver) %>%  dplyr::arrange(sender, .by_group = TRUE)
+  group_data = group_data %>% dplyr::mutate(sender_receiver = factor(sender_receiver, levels = group_data$sender_receiver %>% unique()))
+  
+  group_data_celltype_specificity = prioritization_tables$group_prioritization_tbl  %>% dplyr::mutate(sender_receiver = paste(sender, receiver, sep = " --> "), lr_interaction = paste(ligand, receptor, sep = " - "))  %>% dplyr::distinct(id, sender, receiver, sender_receiver, lr_interaction, group, scaled_pb_ligand, scaled_pb_receptor) %>% dplyr::filter(id %in% sample_data$id) %>%  dplyr::arrange(receiver) %>% dplyr::group_by(receiver) %>%  dplyr::arrange(sender, .by_group = TRUE)
+  group_data_celltype_specificity = group_data_celltype_specificity %>% dplyr::mutate(sender_receiver = factor(sender_receiver, levels = group_data_celltype_specificity$sender_receiver %>% unique()))
+  
+  group_data_frac_expression = prioritization_tables$group_prioritization_table_source  %>% dplyr::mutate(sender_receiver = paste(sender, receiver, sep = " --> "), lr_interaction = paste(ligand, receptor, sep = " - "))  %>% dplyr::distinct(id, sender, receiver, sender_receiver, lr_interaction, group, fraction_ligand_group, fraction_receptor_group) %>% dplyr::filter(id %in% sample_data$id) %>%  dplyr::arrange(receiver) %>% dplyr::group_by(receiver) %>%  dplyr::arrange(sender, .by_group = TRUE)
+  group_data_frac_expression = group_data_frac_expression %>% dplyr::mutate(sender_receiver = factor(sender_receiver, levels = group_data$sender_receiver %>% unique()))
+  
+  omnipath_df = prioritized_tbl_oi %>% distinct(id, curation_effort, n_references, n_resources)
+  
+  group_data_omnipath = prioritization_tables$group_prioritization_table_source  %>% dplyr::mutate(sender_receiver = paste(sender, receiver, sep = " --> "), lr_interaction = paste(ligand, receptor, sep = " - "))  %>% dplyr::distinct(id, sender, receiver, sender_receiver, lr_interaction) %>% dplyr::filter(id %in% sample_data$id) %>% inner_join(omnipath_df) %>%  dplyr::arrange(receiver) %>% dplyr::group_by(receiver) %>%  dplyr::arrange(sender, .by_group = TRUE)
+  group_data_omnipath = group_data_omnipath %>% dplyr::mutate(sender_receiver = factor(sender_receiver, levels = group_data$sender_receiver %>% unique()))
+  
+  group_data = group_data %>% inner_join(group_data_celltype_specificity) %>% inner_join(group_data_frac_expression) 
+  group_data = group_data %>% dplyr::mutate(sender_receiver = factor(sender_receiver, levels = group_data$sender_receiver %>% unique()))
+  rm(group_data_celltype_specificity)
+  rm(group_data_frac_expression)
+
+  keep_sender_receiver_values = c(0.25, 0.9, 1.75, 4)
+  names(keep_sender_receiver_values) = levels(sample_data$keep_sender_receiver)
+  
+  p1 = sample_data %>%
+    ggplot(aes(sample, lr_interaction, color = scaled_LR_pb_prod, size = keep_sender_receiver)) +
+    geom_point() +
+    facet_grid(sender_receiver~group, scales = "free", space = "free", switch = "y") +
+    scale_x_discrete(position = "top") +
+    theme_light() +
+    theme(
+      axis.ticks = element_blank(),
+      axis.title = element_blank(),
+      # axis.title.x = element_text(face = "bold", size = 11),       axis.title.y = element_blank(),
+      axis.text.y = element_text(face = "bold.italic", size = 9),
+      axis.text.x = element_text(size = 9,  angle = 90,hjust = 0),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.spacing.x = unit(0.40, "lines"),
+      panel.spacing.y = unit(0.25, "lines"),
+      strip.text.x.top = element_text(size = 10, color = "black", face = "bold", angle = 0),
+      strip.text.y.left = element_text(size = 9, color = "black", face = "bold", angle = 0),
+      strip.background = element_rect(color="darkgrey", fill="whitesmoke", size=1.5, linetype="solid")
+    ) + labs(color = "Scaled L-R\npseudobulk exprs product", size= "Sufficient presence\nof sender & receiver") + 
+    scale_size_manual(values = keep_sender_receiver_values)
+  max_lfc = abs(sample_data$scaled_LR_pb_prod) %>% max()
+  custom_scale_fill = scale_color_gradientn(colours = RColorBrewer::brewer.pal(n = 7, name = "RdBu") %>% rev(),values = c(0, 0.350, 0.4850, 0.5, 0.5150, 0.65, 1),  limits = c(-1*max_lfc, max_lfc))
+  
+  p1 = p1 + custom_scale_fill
+  
+  p2 = group_data %>%
+    ggplot(aes(direction_regulation , lr_interaction, fill = activity_scaled)) +
+    geom_tile(color = "whitesmoke") +
+    facet_grid(sender_receiver~group, scales = "free", space = "free") +
+    scale_x_discrete(position = "top") +
+    theme_light() +
+    theme(
+      axis.ticks = element_blank(),
+      axis.title = element_blank(),
+      axis.text.y = element_text(face = "bold.italic", size = 9),
+      axis.text.x = element_text(size = 9,  angle = 90,hjust = 0),
+      strip.text.x.top = element_text(angle = 0),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.spacing.x = unit(0.20, "lines"),
+      panel.spacing.y = unit(0.25, "lines"),
+      strip.text.x = element_text(size = 10, color = "black", face = "bold"),
+      strip.text.y = element_blank(),
+      strip.background = element_rect(color="darkgrey", fill="whitesmoke", size=1.5, linetype="solid")
+    ) + labs(fill = "Scaled Ligand\nActivity in Receiver")
+  max_activity = abs(group_data$activity_scaled) %>% max(na.rm = TRUE)
+  custom_scale_fill = scale_fill_gradientn(colours = c("white", RColorBrewer::brewer.pal(n = 7, name = "PuRd") %>% .[-7]),values = c(0, 0.51, 0.575, 0.625, 0.675, 0.725, 1),  limits = c(-1*max_activity, max_activity))
+  
+  p2 = p2 + custom_scale_fill
+  
+  # add the plot visualizing cell-type specificity
+  # cs_data = group_data %>% filter(group %in% prioritized_tbl_oi$group) %>% distinct(sender_receiver, lr_interaction, group, scaled_pb_ligand, scaled_pb_receptor) %>% gather(LR, celltype_specificity, scaled_pb_ligand:scaled_pb_receptor)
+  cs_data = group_data %>% distinct(sender_receiver, lr_interaction, group, scaled_pb_ligand, scaled_pb_receptor) %>% tidyr::gather(LR, celltype_specificity, scaled_pb_ligand:scaled_pb_receptor)
+  cs_data$LR[cs_data$LR == "scaled_pb_ligand"] = "ligand"
+  cs_data$LR[cs_data$LR == "scaled_pb_receptor"] = "receptor"
+  frac_data = group_data %>% distinct(sender_receiver, lr_interaction, group, fraction_ligand_group, fraction_receptor_group) %>% tidyr::gather(LR, fraction_expression, fraction_ligand_group:fraction_receptor_group)
+  frac_data$LR[frac_data$LR == "fraction_ligand_group"] = "ligand"
+  frac_data$LR[frac_data$LR == "fraction_receptor_group"] = "receptor"
+  
+  cs_data = cs_data %>% inner_join(frac_data)
+  
+  p_cs = cs_data %>% 
+    ggplot(aes(LR , lr_interaction, color = celltype_specificity, size = fraction_expression)) +
+    geom_point() +
+    facet_grid(sender_receiver ~ group, scales = "free", space = "free") +
+    scale_x_discrete(position = "top") +
+    theme_light() +
+    viridis::scale_color_viridis() +
+    theme(
+      axis.ticks = element_blank(),
+      axis.title = element_blank(),
+      axis.title.y = element_blank(),
+      axis.text.y = element_blank(),
+      axis.text.x = element_text(size = 9,  angle = 90,hjust = 0),
+      strip.text.x.top = element_text(angle = 0),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.spacing.x = unit(0.20, "lines"),
+      panel.spacing.y = unit(0.25, "lines"),
+      strip.text.x = element_text(size = 10, color = "black", face = "bold"),
+      strip.text.y = element_blank(),
+      strip.background = element_rect(color="darkgrey", fill="whitesmoke", size=1.5, linetype="solid")
+    ) + labs(color = "Scaled celltype specificity") + labs(size = "Fraction of expression")
+  
+  # add the plot visualizing Omnipath DB LR scores
+  group_data = group_data %>% inner_join(group_data_omnipath) 
+  group_data = group_data %>% dplyr::mutate(sender_receiver = factor(sender_receiver, levels = group_data$sender_receiver %>% unique()))
+  
+  omnipath_data = group_data %>% distinct(sender_receiver, lr_interaction, curation_effort, n_references, n_resources) %>% tidyr::gather(omnipath_score_type, omnipath_score, curation_effort:n_resources)
+  omnipath_data$omnipath_score_type[omnipath_data$omnipath_score_type == "curation_effort"] = "curation effort"
+  omnipath_data$omnipath_score_type[omnipath_data$omnipath_score_type == "n_references"] = "nr. references"
+  omnipath_data$omnipath_score_type[omnipath_data$omnipath_score_type == "n_resources"] = "nr. resources"
+  omnipath_data = omnipath_data %>% mutate(omnipath = "Omnipath")
+  p_omnipath = omnipath_data %>% 
+    ggplot(aes(omnipath_score_type , lr_interaction, size = omnipath_score)) +
+    geom_point(color = "grey40") +
+    facet_grid(sender_receiver ~ omnipath, scales = "free", space = "free") +
+    scale_x_discrete(position = "top") +
+    theme_light() +
+    theme(
+      axis.ticks = element_blank(),
+      axis.title = element_blank(),
+      axis.title.y = element_blank(),
+      axis.text.y = element_blank(),
+      axis.text.x = element_text(size = 9,  angle = 90,hjust = 0),
+      strip.text.x.top = element_text(angle = 0),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.spacing.x = unit(0.20, "lines"),
+      panel.spacing.y = unit(0.25, "lines"),
+      strip.text.x = element_text(size = 10, color = "black", face = "bold"),
+      strip.text.y = element_blank(),
+      strip.background = element_rect(color="darkgrey", fill="whitesmoke", size=1.5, linetype="solid")
+    )  + labs(size = "Omnipath DB score")
+  
+  
+  if(!is.null(widths)){
+    p = patchwork::wrap_plots(
+      p1,p2,p_cs, p_omnipath,
+      nrow = 1,guides = "collect",
+      widths = widths
+    )
+  } else {
+    p = patchwork::wrap_plots(
+      p1,p2,p_cs, p_omnipath,
+      nrow = 1,guides = "collect",
+      widths = c(sample_data$sample %>% unique() %>% length(), 2*(sample_data$group %>% unique() %>% length()),2*(sample_data$group %>% unique() %>% length()),3)
+    )
+  }
+  
+  return(p)
+  
+}
+
 #' @title make_sample_lr_prod_activity_batch_plots
 #'
 #' @description \code{make_sample_lr_prod_activity_batch_plots}  Visualize the scaled product of Ligand-Receptor (pseudobulk) expression per sample, and compare the different groups. In addition, show the NicheNet ligand activities in each receiver-celltype combination. On top of this summary plot, a heatmap indicates the batch value for each displayed sample.
